537af3a2b4933d5cd38aa0c86c2bc1b36321cab1
veean <nersesowm@gmail.com>
2016-10-10 12:17:29 +0400

    define load data function : usual example for open json file

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,8 +1,11 @@
-
+import os
 
 def load_data(filepath):
-    pass
-
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        imported_text = file_handler.readlines()
+        return imported_text
 
 def get_most_frequent_words(text):
     pass
